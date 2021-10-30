0e59f5228913df2dd5638773211acfbd65afa32a
merlingja <90874628+merlingja@users.noreply.github.com>
2021-10-30 00:31:18 -0600

    se actualizo tipo producto post delete put get

--- modulos/modulo_inventario/router/tipo_producto.js
+++ modulos/modulo_inventario/router/tipo_producto.js
@@ -1,3 +1,5 @@
+//-----------------------Modulo Inventario--------------------------------------
+//-------------------Creado por Merling Aguilar---------------------------------
 const express = require('express'); 
 const routers = express.Router();
 const conexion = require('../../../config/conexion');
@@ -5,11 +7,11 @@ const conexion = require('../../../config/conexion');
 
 //obtiene todos los productos
 routers.get('/tipo-productos', (req, res) => { 
-conexion.query('SELECT *FROM in_tipo_producto', (err, result) => {
+conexion.query("call MOSTRAR_TIPOS_PRODUCTOS()", (err, result) => {
     if (err) {
         res.status(404).send({message: 'recurso no encontrado'});
     } else {
-        res.status(201).send({productos: result });  
+        res.status(201).send({producto: result });  
     }
 });
 });
@@ -22,13 +24,13 @@ routers.get('/tipo-producto/:id',(req,res)=> {
         if (err){
             res.status(404).send({mensaje: "Error al consultar los datos"});
         } else {
-            res.status(201).send({resultado: result, mensaje: "Peticion Exitosa"});
+            res.status(201).send({producto: result, mensaje: "Peticion Exitosa"});
         }  
       
-})  
-})       
+});
+});      
 
-// Eliminar los datos del empleado en función de la identificación
+// Eliminar los datos del producto
 routers.delete('/eliminar-tipo-producto/:id',(req,res)=>{ 
     var cod_tipo_producto = req.params.id; 
     conexion.query("call BORRAR_TIPO_PRODUCTO('"+cod_tipo_producto+"')",
@@ -36,11 +38,11 @@ routers.delete('/eliminar-tipo-producto/:id',(req,res)=>{
         if (err){
             res.status(404).send({mensaje: "Error al eliminar los datos"});
         } else {
-            res.status(201).send({resultado: result, mensaje: "Eliminación de datos exitosa"});
+            res.status(201).send({producto: result, mensaje: "Eliminación de datos exitosa"});
         }  
       
-})  
-})  
+});  
+});  
   
 //Insertar  un producto a través el procedimiento almacenado
 routers.post('/insertar-tipo-producto', (req, res) => {
@@ -50,14 +52,33 @@ routers.post('/insertar-tipo-producto', (req, res) => {
         conexion.query("call INS_TIPO_PRODUCTO('"+tipo+"','"+descripcion+"')",
         (err, result) => {
             if (err){
-                res.status(404).send({mensaje: "Error al insertar el tipo de producto"});
+                res.status(404).send({mensaje: "Error al insertar el producto"});
             } else {
-                res.status(201).send({resultado: result, mensaje: "Se inserto con exito"});
+                res.status(201).send({producto: result, mensaje: "Se inserto con exito"});
             }
         });
-});    
-// Actualizar un producto a través del procedimiento almacenado
 
-//exporta la ruta que tuvo actividad este archivo  */
+});   
+
+//actualiza tipo de producto
+routers.put('/actualizar-tipo-producto', (req, res) => {
+    let cod_tip_producto = req.body.cod_tip_producto;
+    let tip_producto = req.body.tip_producto;
+    let des_tip_producto = req.body.des_tip_producto;
+    
+      
+        conexion.query("call ACT_TIPO_PRODUCTO('"+cod_tip_producto+"', '"+tip_producto+"','"+des_tip_producto+"')",
+        (err, result) => {
+            if (err){
+                res.status(404).send({mensaje: "Error al actualizar el producto"});
+            } else {
+                res.status(201).send({producto: result, mensaje: "Se actualizo con exito"});
+            }
+        });
+
+});
+
+
+//exporta la ruta que tuvo actividad en este archivo 
 module.exports = routers;
 
